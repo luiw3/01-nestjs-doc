62e1dc2026999e4a5aa7e0bb442737358f61bc7d
Luis Otavio <luisotavioldesouza1@gmail.com>
2021-11-17 15:09:00 -0300

    Implementing basic controllers operations

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,13 +1,15 @@
-import { Controller,Get, Post, Req } from '@nestjs/common';
+import { Controller,Get, Header, HttpCode, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
     @Post()
+    @HttpCode(204)
+    @Header('Cache-Control', 'none')
     create(): string {
         return 'This action adds a new cat';
     }
-    
+
     @Get()
     findAll(@Req() request: Request): string {
         return 'This action returns all cats';
